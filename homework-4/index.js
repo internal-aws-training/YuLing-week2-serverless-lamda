aade53fce6e1d727ba07eda0dc0a6e26af337c2e
Liu Yuling <liu.yuling@hometrack.com.au>
2020-05-12 00:02:11 +0800

    create lamda by s3 but should add updated atuo when s3 upload new file

--- homework-4/index.js
+++ homework-4/index.js
@@ -1,6 +1,22 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+s3.getObject(params, function (err, data)
+{
+  if (err)
+  {
+    console.log(err, err.stack); // an error occurred
+    return null
+  }
+  else
+  {
+    console.log(data);           // successful response
+    return data
+  }
+});
 exports.handler = async function (event, context)
 {
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2))
-  console.log("EVENT\n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
+  var params = { Bucket: 'yuling-s3-01', Key: 'hello.txt' }
+  var s3file = s3.getObject(params)
+
+  return s3file
 }
